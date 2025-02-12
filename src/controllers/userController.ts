5ec36dfed9670196be06e1c8591541d1f5f560b6
amami13 <avisar.amami@gmail.com>
2025-02-12 17:35:55 +0200

    added log

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -103,6 +103,7 @@ const register = async (req: Request, res: Response) => {
               ...tokens
           })
   } catch (err) {
+      console.log(err);
       return res.status(400).send("error missing email or password");
   }
 };
